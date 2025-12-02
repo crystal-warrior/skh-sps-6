e4de7664294ae62c59a6ed7d9d1adbfb9b674277
Sergey Khvostov <sparrowrow34@yandex.ru>
2025-12-02 17:51:29 +0300

    Reorder function definitions in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 import math
 
-def get_area(a, b):
-    return a * b  # пропущено деление на 2
-
 def get_hypotenuse(a, b):
-    return math.sqrt(math.pow(a, 3) + math.pow(b, 3))  # кубы вместо квадратов
+    return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
+
+def get_area(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print("Введите a:")
